aa8ef028b4b48601b0d47785d99f47ded9d47dcd
Rudresh17 <rudreshlegend@gmail.com>
2020-07-16 08:01:42 +0530

    lets do it

--- server.js
+++ server.js
@@ -16,11 +16,11 @@ const io = socketio(server);
 
 if (process.env.NODE_ENV === "production") {
 
-  app.use(express.static("Public"));
+  app.use(express.static("build"));
 
 
   app.get("*", (req, res) => {
-       res.sendFile(path.resolve(__dirname,  "Public", "index.html"));
+       res.sendFile(path.resolve(__dirname,  "build", "index.html"));
    });
 
  }
